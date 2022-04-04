8a446cf9c6f16d7fb3e389255683459dc651bda2
Zerotonine <metalfreak1991@hotmail.de>
2022-04-04 11:46:55 +0200

    new migrations for bundling multiple contents together

---
+++ migrations/2022_04_04_000000_create_h5p_content_container_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateH5pContentContainerTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('h5p_content_container', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('h5p_content_container');
+    }
+}
---
+++ migrations/2022_04_04_000001create_h5p_content_container_contents_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateH5pContentContainerContentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('h5p_content_container_contents', function (Blueprint $table) {
+            $table->bigInteger('container_id')->unsigned();
+            $table->bigInteger('content_id')->unsigned();
+            $table->foreign('container_id')->references('id')->on('h5p_content_container');
+            $table->foreign('content_id')->references('id')->on('h5p_contents');
+            $table->primary(['container_id', 'content_id'], 'fk_primary');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('h5p_content_container_contents');
+    }
+}
