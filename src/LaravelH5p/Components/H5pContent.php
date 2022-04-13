f800da27dc67d93377fc7b7171f6e1cfcc4f5a37
Zerotonine <metalfreak1991@hotmail.de>
2022-04-13 11:30:26 +0200

    added the option to show a title above the content package

--- src/LaravelH5p/Components/H5pContent.php
+++ src/LaravelH5p/Components/H5pContent.php
@@ -4,12 +4,15 @@ namespace EscolaSoft\LaravelH5p\Components;
 
 use Illuminate\View\Component;
 use Illuminate\Support\Facades\App;
+use EscolaSoft\LaravelH5p\Eloquents\H5pContent as Content;
 
 class H5pContent extends Component
 {
     public $contentId = null;
     public $width = null;
     public $height = null;
+    public $showTitle = false;
+    public $title = '';
     // public $settings = null;
     // public $embed_code = null;
 
@@ -18,23 +21,33 @@ class H5pContent extends Component
      *
      * @return void
      */
-    public function __construct($contentId, $width = "1200", $height = "auto")
+    public function __construct($contentId, $showTitle = false, $width = "1200", $height = "auto")
     {
         $this->contentId = $contentId;
         $this->width = $width;
         $this->height = $height;
-        // $this->initialize($contentId);
+        $this->showTitle = $showTitle;
+        $this->initialize($contentId);
     }
 
     protected function initialize($contentId){
-        $h5p = App::make('LaravelH5p');
-        $core = $h5p::$core;
-        $this->settings = $h5p::get_editor();
-        $content = $h5p->get_content($contentId);
-        $embed = $h5p->get_embed($content, $this->settings);
-        $this->embed_code = $embed['embed'];
-        $this->settings = $embed['settings'];
+        if($this->showTitle)
+            $this->title = $this->getTitle($contentId);
+
+        // $h5p = App::make('LaravelH5p');
+        // $core = $h5p::$core;
+        // $this->settings = $h5p::get_editor();
+        // $content = $h5p->get_content($contentId);
+        // $embed = $h5p->get_embed($content, $this->settings);
+        // $this->embed_code = $embed['embed'];
+        // $this->settings = $embed['settings'];
         // $user = Auth::user();
+    }
+
+    protected function getTitle($contentId){
+        if($contentId)
+            return Content::findOrFail(['id' => $contentId])->first()->title;
+        return null;
     }
 
     /**
--- views/components/h5p-content.blade.php
+++ views/components/h5p-content.blade.php
@@ -1,5 +1,12 @@
-    <iframe id="h5p-iframe-{{$contentId}}" src="http://localhost:8000/admin/h5p/h5p/embed/{{$contentId}}" width="{{$width}}" height="{{$height}}" frameborder="0" allowfullscreen>
+<div class="flex flex-col justify-center">
+    @if($showTitle)
+        <h1 class="mx-2 py-1 font-bold text-2xl text-center">{{$title}}</h1>
+    @endif
+    @if($contentId)
+        <iframe id="h5p-iframe-{{$contentId}}" src="http://localhost:8000/admin/h5p/h5p/embed/{{$contentId}}" width="{{$width}}" height="{{$height}}" frameborder="0" allowfullscreen>
+        </iframe>
+        <script src="http://localhost:8000/assets/vendor/h5p/h5p-core/js/h5p-resizer.js" charset="UTF-8"></script>
+    @endif
+</div>
 
-    </iframe>
-    <script src="http://localhost:8000/assets/vendor/h5p/h5p-core/js/h5p-resizer.js" charset="UTF-8"></script>
 
